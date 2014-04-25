bec37bd8a9c5a43d7af8b8be98516648150e9a5e
rc01 <cr.c@outlook.com>
2014-04-25 18:42:47 -0400

    reloading base file for better history tracking

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,15 @@
-## The purpose of the functions is two-fold:
-## (i)  Creating a matrix with the ability of incorporating new values or
-##      retrieving previously recorded values.
-## (ii) Computing the inverse of that matrix or extracting its cached 
-##      when previously stored.
-## These two functions interact with each other.
+## Put comments here that give an overall description of what your
+## functions do
 
-## The "makeCacheMatrix" function set or retrieve matrix values. Also the 
-## function gets or stores the matrix inverse.   
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL                         
-    set <- function(y) {                # set the value of a matrix object
-        x <<- y                         
-        m <<- NULL                       
-    }
-    get <- function() x                 # extract the value from the object
-    setInverse <- function(Inverse) m <<- Inverse  # store the matrix inverse  
-                                                   #value for future use
-    getInverse <- function() m          # extract the matrix inverse value
-    list(set = set, get = get,
-         setInverse = setInverse,
-         getInverse = getInverse)
+    
 }
 
-## The "cacheSolve" function computes the matrix inverse in the case that this 
-## is not already cached.
+
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
-    m <- x$getInverse()                 # query the x matrix's cache         
-    if(!is.null(m)) {                   # if there is a cache
-        message("getting cached data") 
-        return(m)                       # return the cache w/o computation
-    }
-    data <- x$get()                     # if there's no cache
-    m <- solve(data, ...)               # matrix inversion computation here
-    x$setInverse(m)                     # save the result back to x's cache
-    m                                   # return the result
+    ## Return a matrix that is the inverse of 'x'
 }
